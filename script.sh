d7ba75ff3feda409738ae8405b6b8a220af315e5
gabe <gabe@thesqrtminus1.com>
2016-04-08 05:31:58 +0000

    added script

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Please enter the name of the channel you wish to download:"
+read ChannelName
+echo "You entered: $ChannelName for the channel"
+sleep 1
+echo "Now downloading all videos from Channel "$ChannelName" (if it exists!)"
+youtube-dl -iw --no-continue ytuser:$ChannelName --add-metadata -f bestvideo+bestaudio --merge-output-format mkv -o $ChannelName/"[%(upload_date)s] %(title)s"
+#cd $ChannelName
+#for f in ./*.mkv; do
+#touch -r "$f" "${f%.mkv}".attributes
+#ffmpeg -i "$f" -c:v libx264 -crf 18 -preset medium -strict experimental -c:a aac -movflags +faststart "${f%.mkv}.mp4"
+#rm -rf "$f"
+#touch -r "${f%.mkv}".attributes "${f%.mkv}.mp4"
+#sudo rm -rf "${f%.mkv}".attributes;
+#done
+exit 0
+
