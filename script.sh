e2a2ac64aa2cc8beceb1e4fab8d51fc3cf5130f4
gabe <thesqrtminus1@gmail.com>
2016-05-18 21:36:01 +0000

    updated script so that you can specify the $uploadernameandid variable for playlists where all the uploaders aren't the same (like for "lost video archives" that you can find on youtube)

--- script.sh
+++ script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-while getopts :u:h:d: option
+while getopts :u:h:d:n: option
 do
         case "${option}"
                 in
                         u) URL=${OPTARG};;
-                        h) echo "-c for channel name"
-                                exit 1;;
 			d) DIR=${OPTARG};;
-                        \?) echo "that's not a flag, you dope."
+                        h) echo "-u for URL, -d for directory, -n for uploader and id (formatted uploader{uploaderid}"
+                                exit 1;;
+			n) uploaderandid=${OPTARG};;
+                        \?) echo "that's not a flag, you dope. Maybe try -h"
                                 exit 1;;
 
         esac
@@ -17,20 +18,25 @@ if [[ $URL == "" || $DIR == "" ]] ;
                 echo "You need to input a URL and a directory to which to download, numbnuts."
                 exit 1
         else
-
 echo "You entered: $URL for the URL"
-uploader="$(youtube-dl -i -J $URL --playlist-items 1 | grep -Po '(?<="uploader": ")[^"]*')"
-uploader_id="$(youtube-dl -i -J $URL --playlist-items 1 | grep -Po '(?<="uploader_id": ")[^"]*')"
-uploaderandid="$uploader{$uploader_id}"
-echo "Uploader: $uploader"
-echo "Uploader ID: $uploader_id"
-echo "Folder Name: $uploaderandid"
+		if [[ $uploaderandid == "" ]] ;
+        then
+		uploader="$(youtube-dl -i -J $URL --playlist-items 1 | grep -Po '(?<="uploader": ")[^"]*')"
+		uploader_id="$(youtube-dl -i -J $URL --playlist-items 1 | grep -Po '(?<="uploader_id": ")[^"]*')"
+		uploaderandid="$uploader{$uploader_id}"
+		echo "Uploader: $uploader"
+		echo "Uploader ID: $uploader_id"
+		echo "Folder Name: $uploaderandid"
+        else
+		echo "Folder Name: $uploaderandid"
+fi
 echo "Now downloading all videos from URL "$URL" to the folder "$DIR/$uploaderandid""
 cd $DIR && cd "$uploaderandid" || mkdir -p $DIR && cd $DIR && mkdir -p "$uploaderandid" && cd "$uploaderandid"
 youtube-dl -iw \
 --no-continue $URL \
 -f bestvideo+bestaudio --merge-output-format mkv \
--o "[%(upload_date)s] %(title)s" \
---add-metadata --download-archive archive.txt
+-o "[%(upload_date)s] %(title)s" --prefer-ffmpeg --recode-video mp4 \
+--add-metadata --download-archive archive.txt \
+--postprocessor-args "-c:v libx264 -crf 18 -preset medium -strict experimental -c:a aac -movflags +faststart -loglevel 32"
 fi
 exit 0
